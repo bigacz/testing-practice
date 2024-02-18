299615dd759af7fd6078105d4700fe0c8cc83362
bigacz <sbigacz@gmail.com>
2024-02-18 17:26:42 +0100

    add capitalize solution

---
+++ capitalize/capitalize.js
@@ -0,0 +1,15 @@
+function capitalize(string) {
+  if (typeof string !== 'string' || string.length < 1) {
+    return;
+  }
+
+  const stringArray = string.split('');
+  const capitalizedFirst = stringArray[0].toUpperCase();
+
+  const capitalizedArray = stringArray.toSpliced(0, 1, capitalizedFirst);
+  const capitalizedWord = capitalizedArray.join('');
+
+  return capitalizedWord;
+}
+
+export default capitalize;
---
+++ capitalize/capitalize.test.js
@@ -0,0 +1,19 @@
+import capitalize from './capitalize';
+
+test('capitalize word', () => {
+  expect(capitalize('word')).toBe('Word');
+  expect(capitalize('hello World!')).toBe('Hello World!');
+  expect(capitalize('.what')).toBe('.what');
+});
+
+test('wrong input type', () => {
+  const word = capitalize(12345);
+
+  expect(word).toBe(undefined);
+});
+
+test('empty word', () => {
+  const word = capitalize('');
+
+  expect(word).toBe(undefined);
+});
